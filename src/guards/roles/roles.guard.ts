ebbe3d73e24d5867bea9b66942fa8aeb27ed90ab
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2021-03-12 20:35:28 -0300

    feat(roles.guard.ts): created the RolesGuard that validates the roles saved in the metadata

---
+++ src/guards/roles/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+
+import { RequestUser } from 'src/utils/type.shared'
+
+/**
+ * Guard that protects some route testing the roles saved in the metadata
+ */
+export class RolesGuard implements CanActivate {
+  public canActivate(context: ExecutionContext): boolean {
+    const roles = new Reflector().get<string[]>('roles', context.getHandler())
+
+    if (!roles) return true
+
+    const user: RequestUser = context.switchToHttp().getRequest().user
+
+    if (!user)
+      throw new UnauthorizedException(
+        'You have no permission to access those sources'
+      )
+
+    const hasRole = user.roles.split('|').some(role => roles.includes(role))
+
+    if (user.roles && hasRole) return true
+
+    if (!user)
+      throw new ForbiddenException(
+        'You have no permission to access those sources'
+      )
+  }
+}
