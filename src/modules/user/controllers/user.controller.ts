35b870be56eae44786676d5fc92c0d82bde81538
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2021-03-12 23:55:55 -0300

    feat(user.controller.ts & user.module.ts): setup UserEntity in UserModule

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 
 /**
  * The app's main user controller class
  *
  * Class that deals with the user routes
  */
+@ApiTags('users')
 @Controller('users')
 export class UserController {}
