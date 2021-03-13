99ab9f137768054901e318ee3a69f232e2fbeefe
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2021-03-12 23:54:23 -0300

    feat(app.module.ts): imported the UserModule and AuthModule; setup TypeOrmModule; fixed the .env path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AuthModule } from './modules/auth/auth.module'
+import { UserModule } from './modules/user/user.module'
 
 @Module({
   imports: [
+    UserModule,
+    AuthModule,
+    TypeOrmModule.forRoot(),
     ConfigModule.forRoot({
-      envFilePath: ['.env']
+      envFilePath: ['.env.development']
     })
   ]
 })
