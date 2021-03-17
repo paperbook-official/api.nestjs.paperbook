b1bc96be3d7aee6b61e7f008ef054268eca3623c
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-17 20:20:43 -0300

    style(to-proxy.ts): added some comments in this interface

--- src/common/to-proxy.ts
+++ src/common/to-proxy.ts
@@ -2,5 +2,9 @@
  * Interface that is used to convert some entity to proxy
  */
 export interface ToProxy<TProxy> {
+  /**
+   * Method that converts the entity to you proxy
+   * @returns the proxy data
+   */
   toProxy(...params: unknown[]): TProxy
 }
