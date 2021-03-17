d8ddf81326c4e6a6bd9008754dea451b638e90b8
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-16 21:36:23 -0300

    feat(user.controller.ts & user.service.ts): created the "create", "get" and "getMe" methods

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
-import { Crud } from '@nestjsx/crud'
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common'
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags
+} from '@nestjs/swagger'
+import { Crud, CrudRequest, ParsedRequest } from '@nestjsx/crud'
+
+import { User } from 'src/decorators/user/user.decorator'
+
+import { JwtGuard } from 'src/guards/jwt/jwt.guard'
 
 import { UserEntity } from '../entities/user.entity'
+
+import { CreateUserPayload } from '../models/create-user.payload'
+import { UserProxy } from '../models/user.proxy'
+
+import { UserService } from '../services/user.service'
+
+import { RequestUser } from 'src/utils/type.shared'
 
 /**
  * The app's main user controller class
@@ -14,9 +32,115 @@ import { UserEntity } from '../entities/user.entity'
     type: UserEntity
   },
   routes: {
-    only: ['getOneBase', 'getManyBase']
+    only: ['getManyBase']
   }
 })
 @ApiTags('users')
 @Controller('users')
-export class UserController {}
+export class UserController {
+  public constructor(private readonly userService: UserService) {}
+
+  /**
+   * Method that is called when the user access the "/user" route with the "POST"
+   * @param createdUserPayload stores the new user data
+   * @returns the created user data
+   */
+  @ApiOperation({ summary: 'Creates a new user' })
+  @ApiCreatedResponse({
+    description: 'Gets the created user data',
+    type: UserProxy
+  })
+  @Post()
+  public async create(
+    @Body() createdUserPayload: CreateUserPayload
+  ): Promise<UserProxy> {
+    const entity = await this.userService.create(createdUserPayload)
+    return entity.toProxy()
+  }
+
+  /**
+   * Method that is called when the user access the "/user/me" route with "GET" method
+   * @param requestUser stores the logged user data
+   * @param crudRequest stores the joins, filters, etc
+   * @returns the logged user data
+   */
+  @ApiQuery({
+    required: false,
+    name: 'cache',
+    type: 'integer',
+    description: 'Reset cache (if was enabled).'
+  })
+  @ApiQuery({
+    required: false,
+    name: 'fields',
+    type: 'string',
+    isArray: true,
+    description: 'Selects resource fields.'
+  })
+  @ApiQuery({
+    required: false,
+    name: 'join',
+    type: 'string',
+    isArray: true,
+    description: 'Adds relational resources.'
+  })
+  @ApiOperation({ summary: 'Gets the logged user' })
+  @ApiOkResponse({ description: 'Gets the logged user data', type: UserProxy })
+  @UseGuards(JwtGuard)
+  @Get('me')
+  public async getMe(
+    @User() requestUser: RequestUser,
+    @ParsedRequest() crudRequest: CrudRequest
+  ): Promise<UserProxy> {
+    const entity = await this.userService.get(
+      requestUser.id,
+      requestUser,
+      crudRequest
+    )
+    return entity.toProxy()
+  }
+
+  /**
+   * Method that is called when the user access the "/user/:userId" route with "GET" method
+   * @param userId stores the user id
+   * @param requestUser stores the logged user data
+   * @param crudRequest stores the joins, filters, etc
+   * @returns the found user data
+   */
+  @ApiQuery({
+    required: false,
+    name: 'cache',
+    type: 'integer',
+    description: 'Reset cache (if was enabled).'
+  })
+  @ApiQuery({
+    required: false,
+    name: 'fields',
+    type: 'string',
+    isArray: true,
+    description: 'Selects resource fields.'
+  })
+  @ApiQuery({
+    required: false,
+    name: 'join',
+    type: 'string',
+    isArray: true,
+    description: 'Adds relational resources.'
+  })
+  @ApiParam({
+    name: 'userId',
+    type: 'number'
+  })
+  @ApiOperation({ summary: 'Gets the user by id' })
+  @ApiOkResponse({ description: 'Gets the user data' })
+  @UseGuards(JwtGuard)
+  @Get(':userId')
+  public async get(
+    @Param('userId') userId: number,
+    @User() requestUser: RequestUser,
+    @ParsedRequest() crudRequest: CrudRequest
+  ): Promise<UserProxy> {
+    const entity = await this.userService.get(userId, requestUser, crudRequest)
+    return entity.toProxy()
+  }
+}
--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { CrudRequest } from '@nestjsx/crud'
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
 
 import { UserEntity } from '../entities/user.entity'
+
+import { CreateUserPayload } from '../models/create-user.payload'
+
+import { encryptPassword } from 'src/utils/password'
+import { RequestUser } from 'src/utils/type.shared'
+import { isAdminUser } from 'src/utils/validations'
+
+import { RolesEnum } from 'src/models/enums/roles.enum'
 
 /**
  * The app's main user service class
@@ -18,4 +31,80 @@ export class UserService extends TypeOrmCrudService<UserEntity> {
   ) {
     super(repository)
   }
+
+  /**
+   * Method that can save some entity in the database
+   * @param createUserPayload stores the new user data
+   * @returns the created user entity
+   */
+  public async create(
+    createUserPayload: CreateUserPayload
+  ): Promise<UserEntity> {
+    const entity = new UserEntity(createUserPayload)
+
+    entity.password = await encryptPassword(entity.password)
+    entity.roles = entity.roles ?? RolesEnum.User
+
+    return await entity.save()
+  }
+
+  /**
+   * Method that can get one user entity
+   * @param userId stores the user id
+   * @param requestUser stores the logged user data
+   * @param crudRequest stores the joins, filters, etc
+   * @returns the found user data
+   */
+  public async get(
+    userId: number,
+    requestUser: RequestUser,
+    crudRequest?: CrudRequest
+  ): Promise<UserEntity> {
+    let entity: UserEntity
+
+    if (!crudRequest) {
+      entity = await UserEntity.findOne({ id: userId })
+    } else {
+      crudRequest.parsed.search = {
+        $and: [
+          {
+            isActive: true
+          },
+          ...crudRequest.parsed.search.$and
+        ]
+      }
+
+      entity = await super.getOne(crudRequest)
+    }
+
+    if (!entity || !entity.isActive) {
+      throw new NotFoundException(
+        `The entity identified by "${userId}" does not exist or is disabled`
+      )
+    }
+
+    if (!UserService.hasPermissions(entity, requestUser))
+      throw new ForbiddenException(
+        'You have no permission to access those sources'
+      )
+
+    return entity
+  }
+
+  //#region Utils
+
+  /**
+   * Method that validates if the user can access some source
+   * @param entity stores the found entity
+   * @param requestUser stores the logged user
+   * @returns true if the user can access some source, otherwise false
+   */
+  private static hasPermissions(
+    entity: UserEntity,
+    requestUser: RequestUser
+  ): boolean {
+    return entity.id === requestUser.id || isAdminUser(requestUser)
+  }
+
+  //#endregion
 }
