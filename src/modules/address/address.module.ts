7d14d5b2e4a5224eb08dae5b989237329dbc20d7
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-17 22:37:08 -0300

    feat(address.module.ts): added the "AddressEntity" typeorm reference in "AddressModule" imports

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AddressEntity } from './entities/address.entity'
 
 import { AddressService } from './services/address.service'
 
@@ -7,7 +10,7 @@ import { AddressController } from './controllers/address.controller'
 import { UserModule } from '../user/user.module'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forFeature([AddressEntity])],
   controllers: [AddressController],
   providers: [AddressService],
   exports: [AddressService]
