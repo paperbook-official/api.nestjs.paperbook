ee0c0b14d8aa10f6dd16004605416650afa2f378
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-17 21:02:03 -0300

    feat(create-address.payload.ts & update-address.payload.ts): finished the address payloads

---
+++ src/modules/address/models/update-address.payload.ts
@@ -0,0 +1,55 @@
+import { ApiPropertyOptional } from '@nestjs/swagger'
+
+import {
+  IsNumber,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  Length
+} from 'class-validator'
+import { DefaultValidationMessages } from 'src/models/enums/default-validation-messages.enum'
+
+/**
+ * The app's main update address payload class
+ *
+ * Class that handles the payload sent by the user to perform the update
+ */
+export class UpdatedAddressPayload {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumberString({}, { message: 'It is required to send a numeric string' })
+  @Length(8)
+  public cep?: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  public street?: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  @IsNumber({ maxDecimalPlaces: 0 })
+  public houseNumber?: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  public complement?: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  public district?: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  public city?: string
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  @Length(2)
+  public state?: string
+}
