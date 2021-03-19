8f226a39c8eb2ce0702b2bbc86c56cc8d2107ab5
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-19 20:11:41 -0300

    feat(update-product.payload.ts): created the files main properties

---
+++ src/modules/product/models/update-product.payload.ts
@@ -0,0 +1,67 @@
+import { ApiPropertyOptional } from '@nestjs/swagger'
+
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min
+} from 'class-validator'
+import { DefaultValidationMessages } from 'src/models/enums/default-validation-messages.enum'
+
+/**
+ * The app's main create product payload class
+ *
+ * Class that handles the payload sent by the user peform the update
+ */
+export class UpdateProductPayload {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: DefaultValidationMessages.IsString })
+  @IsNotEmpty({ message: DefaultValidationMessages.IsString })
+  public name: string
+
+  @ApiPropertyOptional()
+  @IsDefined({ message: 'It is required to send the description' })
+  @IsString({ message: DefaultValidationMessages.IsString })
+  public description: string
+
+  @ApiPropertyOptional()
+  @IsDefined({ message: 'It is required to send the full price' })
+  @IsNumber({}, { message: DefaultValidationMessages.IsNumber })
+  @Min(0)
+  public fullPrice: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber({}, { message: DefaultValidationMessages.IsNumber })
+  @Min(0)
+  public installmentPrice?: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: DefaultValidationMessages.IsNumber }
+  )
+  @Min(0)
+  public installmentAmount?: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber({}, { message: DefaultValidationMessages.IsNumber })
+  @Max(1)
+  @Min(0)
+  public discountAmount?: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: DefaultValidationMessages.IsNumber }
+  )
+  @Min(1)
+  public stockAmount: number
+}
