72b9d425dbe213429758b946d8612b21c556f344
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-22 20:22:25 -0300

    feat(product.entity.ts): added "ApiProperty" in all the properties of "ProductEntity"

--- src/modules/product/entities/product.entity.ts
+++ src/modules/product/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { Column, Entity, ManyToOne } from 'typeorm'
 
 import { BaseEntity } from 'src/common/base.entity'
@@ -7,10 +7,16 @@ import { UserEntity } from 'src/modules/user/entities/user.entity'
 import { ProductProxy } from '../models/product.proxy'
 import { ToProxy } from 'src/common/to-proxy.interface'
 
+/**
+ * The app's main product entity class
+ *
+ * Class that represents the entity that deals with products
+ */
 @Entity('products')
 export class ProductEntity extends BaseEntity implements ToProxy<ProductProxy> {
   //#region Columns
 
+  @ApiProperty()
   @Column({
     type: 'varchar',
     length: 30,
@@ -19,24 +25,28 @@ export class ProductEntity extends BaseEntity implements ToProxy<ProductProxy> {
   })
   public name: string
 
+  @ApiProperty()
   @Column({
     type: 'text',
     nullable: false
   })
   public description: string
 
+  @ApiProperty()
   @Column({
     type: 'float',
     nullable: false
   })
   public fullPrice: number
 
+  @ApiPropertyOptional()
   @Column({
     type: 'float',
     nullable: true
   })
   public installmentPrice?: number
 
+  @ApiPropertyOptional()
   @Column({
     type: 'int',
     nullable: true,
@@ -44,6 +54,7 @@ export class ProductEntity extends BaseEntity implements ToProxy<ProductProxy> {
   })
   public installmentAmount?: number
 
+  @ApiPropertyOptional()
   @Column({
     type: 'float',
     nullable: true,
@@ -51,12 +62,14 @@ export class ProductEntity extends BaseEntity implements ToProxy<ProductProxy> {
   })
   public discountAmount?: number
 
+  @ApiProperty()
   @Column({
     type: 'int',
     nullable: false
   })
   public stockAmount: number
 
+  @ApiProperty()
   @Column({
     type: 'integer',
     nullable: false
@@ -82,7 +95,7 @@ export class ProductEntity extends BaseEntity implements ToProxy<ProductProxy> {
   }
 
   /**
-   * Method that converts the entity to you proxy
+   * Method that converts the entity to your proxy
    * @returns the proxy data
    */
   public toProxy(): ProductProxy {
