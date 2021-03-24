701882390f25e2d7a78951bdc33bca70a7654bc0
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-03-24 00:17:47 -0300

    refactor(typeorm-config/*): renamed the typeorm config folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-import { TypeOrmConfigService } from './modules/typeorm/services/typeorm-config.service'
+import { TypeOrmConfigService } from './modules/typeorm-config/services/typeorm-config.service'
 
 import { AddressModule } from './modules/address/address.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { OrderModule } from './modules/order/order.module'
 import { ProductModule } from './modules/product/product.module'
 import { TypeOrmConfigModule } from './modules/typeorm/typeorm-config.module'
-import { UserModule } from './modules/user/user.module'
+import { UserModule } from './modules./modules/typeorm-config/typeorm-config.module
 
 @Module({
   imports: [
