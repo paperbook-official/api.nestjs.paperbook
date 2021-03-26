4c17688639e6fd1668e3090ab067c2554473d442
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2021-03-26 00:11:24 -0300

    feat(product-category/models/*): finished the proxies and payloads of this entity

--- src/modules/product-category/models/product-category.proxy.ts
+++ src/modules/product-category/models/product-category.proxy.ts
@@ -1,9 +1,37 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+
 import { ProductCategoryEntity } from '../entities/product-category.entity'
 
 import { BaseProxy } from 'src/common/base.proxy'
+import { CategoryProxy } from 'src/modules/category/models/category.proxy'
+import { ProductProxy } from 'src/modules/product/models/product.proxy'
 
+/**
+ * The app's main product category proxy clas
+ *
+ * Class that deals with the product category return data
+ */
 export class ProductCategoryProxy extends BaseProxy {
+  @ApiProperty()
+  public productId: number
+
+  @ApiProperty()
+  public categoryId: number
+
+  @ApiPropertyOptional({ type: () => ProductProxy })
+  public product?: ProductProxy
+
+  @ApiPropertyOptional({ type: () => CategoryProxy })
+  public category?: CategoryProxy
+
   public constructor(entity: ProductCategoryEntity) {
     super(entity)
+
+    this.productId = entity.productId
+    this.categoryId = entity.productId
+
+    // relations
+    this.product = entity.product?.toProxy()
+    this.category = entity.category?.toProxy()
   }
 }
--- src/modules/product-category/models/update-product-category.payload.ts
+++ src/modules/product-category/models/update-product-category.payload.ts
@@ -1,1 +1,27 @@
-export class UpdateProductCategoryPayload {}
+import { ApiPropertyOptional } from '@nestjs/swagger'
+
+import { IsNumber, IsOptional } from 'class-validator'
+import { DefaultValidationMessages } from 'src/models/enums/default-validation-messages.enum'
+
+/**
+ * The app's main update product category class
+ *
+ * Class that handles the payload sent by the user to perform the update
+ */
+export class UpdateProductCategoryPayload {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: DefaultValidationMessages.IsNumber }
+  )
+  public productId: number
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: DefaultValidationMessages.IsNumber }
+  )
+  public categoryId: number
+}
