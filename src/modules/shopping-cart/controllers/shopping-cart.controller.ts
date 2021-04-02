5a0e289ec703b16c88d5402308f68254145e7593
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-04-02 11:55:42 -0300

    feat(shopping-cart.controller.ts & shopping-cart.service.ts): created the "create" method

--- src/modules/shopping-cart/controllers/shopping-cart.controller.ts
+++ src/modules/shopping-cart/controllers/shopping-cart.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, UseInterceptors } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common'
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Crud, CrudRequestInterceptor } from '@nestjsx/crud'
 
+import { ProtectTo } from 'src/decorators/protect-to/protect-to.decorator'
+import { User } from 'src/decorators/user/user.decorator'
+
+import { CreateShoppingCartPayload } from '../models/create-shopping-cart.payload'
 import { ShoppingCartProxy } from '../models/shopping-cart.proxy'
+
+import { ShoppingCartService } from '../services/shopping-cart.service'
+
+import { RequestUser } from 'src/utils/type.shared'
+
+import { RolesEnum } from 'src/models/enums/roles.enum'
 
 /**
  * The app's main shopping cart controller class
@@ -33,4 +43,34 @@ import { ShoppingCartProxy } from '../models/shopping-cart.proxy'
 @UseInterceptors(CrudRequestInterceptor)
 @ApiTags('shopping-carts')
 @Controller('shopping-carts')
-export class ShoppingCartController {}
+export class ShoppingCartController {
+  public constructor(
+    private readonly shoppingCartService: ShoppingCartService
+  ) {}
+
+  /**
+   * Method that is called when the user acces the "/shopping-cart"
+   * route with the "POST" method
+   * @param requestUser stores the logged user data
+   * @param createShoppingCartPayload stores the new shopping
+   * cart entity data
+   * @returns the created shopping cart entity proxy
+   */
+  @ApiOperation({ summary: 'Creates a new shopping cart entity' })
+  @ApiCreatedResponse({
+    description: 'Gets the created shopping cart entity data',
+    type: ShoppingCartProxy
+  })
+  @ProtectTo(RolesEnum.User, RolesEnum.Seller, RolesEnum.Admin)
+  @Post()
+  public async create(
+    @User() requestUser: RequestUser,
+    @Body() createShoppingCartPayload: CreateShoppingCartPayload
+  ): Promise<ShoppingCartProxy> {
+    const entity = await this.shoppingCartService.create(
+      requestUser,
+      createShoppingCartPayload
+    )
+    return entity.toProxy()
+  }
+}
--- src/modules/shopping-cart/services/shopping-cart.service.ts
+++ src/modules/shopping-cart/services/shopping-cart.service.ts
@@ -5,14 +5,50 @@ import { Repository } from 'typeorm'
 
 import { ShoppingCartEntity } from '../entities/shopping-cart.entity'
 
+import { CreateShoppingCartPayload } from '../models/create-shopping-cart.payload'
+
+import { ProductService } from 'src/modules/product/services/product.service'
+import { UserService } from 'src/modules/user/services/user.service'
+
+import { RequestUser } from 'src/utils/type.shared'
+
 @Injectable()
 export class ShoppingCartService extends TypeOrmCrudService<
   ShoppingCartEntity
 > {
   public constructor(
     @InjectRepository(ShoppingCartEntity)
-    private readonly repository: Repository<ShoppingCartEntity>
+    private readonly repository: Repository<ShoppingCartEntity>,
+    private readonly userService: UserService,
+    private readonly productService: ProductService
   ) {
     super(repository)
+  }
+
+  /**
+   * Method that can create a new shopping cart entity
+   * @param requestUser stores the logged user data
+   * @param createShoppingCartPayload stores the new shopping
+   * cart entity data
+   * @returns the created shopping cart entity
+   */
+  public async create(
+    requestUser: RequestUser,
+    createShoppingCartPayload: CreateShoppingCartPayload
+  ): Promise<ShoppingCartEntity> {
+    const { productId, userId } = createShoppingCartPayload
+
+    /* If there are no products or users with the passed id those
+    services will throw "EntityNotFoundException", if the request
+    user has no permission the "UserService" will throw "ForbiddenException" */
+
+    const product = await this.productService.get(productId)
+    const user = await this.userService.get(userId, requestUser)
+
+    return await new ShoppingCartEntity({
+      ...createShoppingCartPayload,
+      product,
+      user
+    }).save()
   }
 }
