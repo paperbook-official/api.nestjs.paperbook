11b66535bd445c8e6466670f6821b7af29815ec8
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-04-14 20:04:04 -0300

    docs(firebase.service.ts): added some comments in this class

--- src/modules/firebase/services/firebase.service.ts
+++ src/modules/firebase/services/firebase.service.ts
@@ -4,12 +4,23 @@ import { ConfigService } from '@nestjs/config'
 import admin from 'firebase-admin'
 import { v4 as uuidv4 } from 'uuid'
 
+/**
+ * The app's firebase service class
+ *
+ * Class that deals with the firebase actions
+ */
 @Injectable()
 export class FirebaseService implements OnModuleInit {
+  /**
+   * The firebase storage reference
+   */
   public storage: admin.storage.Storage
 
   public constructor(private readonly configService: ConfigService) {}
 
+  /**
+   * Method that is called after the nest created the component
+   */
   public onModuleInit(): void {
     this.storage = admin
       .initializeApp({
@@ -25,6 +36,11 @@ export class FirebaseService implements OnModuleInit {
       .storage()
   }
 
+  /**
+   * Method that can upload some file to the firebase storage
+   * @param multerFile stores the multer file data
+   * @returns the created
+   */
   public async upload(multerFile: Express.Multer.File): Promise<string> {
     return new Promise<string>((res, err) => {
       const filename = uuidv4()
