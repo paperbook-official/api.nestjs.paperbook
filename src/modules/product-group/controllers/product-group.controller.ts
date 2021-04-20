67e10e8223a76c8b777000123ba69b5e490c5203
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-04-19 22:49:37 -0300

    feat(product-group.controller.ts & product-group.service.ts): created the "created" method

--- src/modules/product-group/controllers/product-group.controller.ts
+++ src/modules/product-group/controllers/product-group.controller.ts
@@ -1,10 +1,18 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Post } from '@nestjs/common'
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Crud } from '@nestjsx/crud'
 
+import { ProtectTo } from 'src/decorators/protect-to/protect-to.decorator'
+import { RequestUser } from 'src/decorators/user/user.decorator'
+
+import { UserEntity } from 'src/modules/user/entities/user.entity'
+
+import { CreateProductGroupDto } from '../models/create-product-group.dto'
 import { ProductGroupDto } from '../models/product-group.dto'
 
 import { ProductGroupService } from '../services/product-group.service'
+
+import { RolesEnum } from 'src/models/enums/roles.enum'
 
 /**
  * The app's main product group controller class
@@ -24,10 +32,35 @@ import { ProductGroupService } from '../services/product-group.service'
     }
   }
 })
-@ApiTags('product-group')
-@Controller('product-group')
+@ApiTags('product-groups')
+@Controller('product-groups')
 export class ProductGroupController {
   public constructor(
     private readonly productGroupService: ProductGroupService
   ) {}
+
+  /**
+   * Method that is called when the user access the "/product-groups" with
+   * the "POST" method
+   * @param requestUser stores the logged user data
+   * @param createProductGroupPayload stores the new product group data
+   * @returns the created product group entity dto
+   */
+  @ApiOperation({ summary: 'Creates a new product group' })
+  @ApiCreatedResponse({
+    description: 'Gets the created product group',
+    type: ProductGroupDto
+  })
+  @ProtectTo(RolesEnum.User, RolesEnum.Seller, RolesEnum.Admin)
+  @Post()
+  public async create(
+    @RequestUser() requestUser: UserEntity,
+    @Body() createProductGroupPayload: CreateProductGroupDto
+  ): Promise<ProductGroupDto> {
+    const entity = await this.productGroupService.create(
+      requestUser,
+      createProductGroupPayload
+    )
+    return entity.toDto()
+  }
 }
--- src/modules/product-group/services/product-group.service.ts
+++ src/modules/product-group/services/product-group.service.ts
@@ -4,6 +4,12 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
 
 import { ProductGroupEntity } from '../entities/product-group.entity'
+import { UserEntity } from 'src/modules/user/entities/user.entity'
+
+import { CreateProductGroupDto } from '../models/create-product-group.dto'
+
+import { ProductService } from 'src/modules/product/services/product.service'
+import { ShoppingCartService } from 'src/modules/shopping-cart/services/shopping-cart.service'
 
 /**
  * The app's main product group service clas
@@ -16,8 +22,39 @@ export class ProductGroupService extends TypeOrmCrudService<
 > {
   public constructor(
     @InjectRepository(ProductGroupEntity)
-    repository: Repository<ProductGroupEntity>
+    repository: Repository<ProductGroupEntity>,
+    private readonly productService: ProductService,
+    private readonly shoppingCartService: ShoppingCartService
   ) {
     super(repository)
+  }
+
+  /**
+   * Method that can create a new product group entity
+   * @param requestUser stores the logged user data
+   * @param createProductGroupPayload stores the new product group data
+   * @returns the created product group entity
+   */
+  public async create(
+    requestUser: UserEntity,
+    createProductGroupPayload: CreateProductGroupDto
+  ): Promise<ProductGroupEntity> {
+    const { productId, shoppingCartId } = createProductGroupPayload
+
+    /* If there are no products or shopping carts with the passed id those
+    services will throw "EntityNotFoundException", if the request
+    user has no permission the "UserService" will throw "ForbiddenException" */
+
+    const product = await this.productService.get(productId)
+    const shoppingCart = await this.shoppingCartService.get(
+      shoppingCartId,
+      requestUser
+    )
+
+    return await new ProductGroupEntity({
+      ...createProductGroupPayload,
+      product,
+      shoppingCart
+    }).save()
   }
 }
